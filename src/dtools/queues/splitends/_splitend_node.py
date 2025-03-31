e3381c7c320c7084043a6a7f8369b1538a4ba9b9
grscheller <geoffrey@scheller.com>
2025-03-30 20:06:56 -0600

    dtools.datastructures was becoming a mono-repo
    - or was always a mono-repo
    - development version 0.26.0.0
      - aiming at a 0.26.1 PyPI release
    - split out dtools.datastructures.queues to dtools.queues.restrictive
    - split out dtools.datastructures.splitends to dtools.queues.splitends
    - removed dtools.datastructures.nodes as a separate module
      - was way too ambitious
      - probably better graph packages on PyPI
      - should only attempt if I become really interested in graph theory
    - did roll in class SL_Node into splitends subpackage as class _SENode
      - dtools.queues.splitends._splitend_node._SENode
      - allowed me to customize it a bit for the splitends use case

---
+++ src/dtools/queues/splitends/_splitend_node.py
@@ -0,0 +1,114 @@
+# Copyright 2023-2025 Geoffrey R. Scheller
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+### Data nodes used privately by class SplitEnd
+
+Node classes used with graph-like data structures. API designed to be used by
+other data structures which contain these data structures.
+
+"""
+
+from __future__ import annotations
+from collections.abc import Callable, Iterator
+from typing import cast, TypeVar
+from dtools.fp.err_handling import MB
+
+D = TypeVar('D')  # Not needed for mypy, hint for pdoc.
+T = TypeVar('T')
+
+
+class _SENode[D]:
+    """Data node for class SplitEnd 
+
+    * data node for a top-to-root singularly linked list.
+    * designed so multiple splitends can safely share the same data
+    * this type of node always
+      * contain data
+      * potential link to previous node
+    * nodes point towards a unique "bottom node" with no predecessor
+      * in a Boolean context returns true if not at the bottom
+      * multiple bottom nodes can exist
+    * two nodes compare as equal if
+      * both their previous Nodes are the same
+      * their data compares as equal
+    * more than one node can point to the same proceeding node
+      * forming bush like graphs
+
+    """
+
+    __slots__ = '_data', '_prev'
+
+    def __init__(self, data: D, prev: MB[_SENode[D]]) -> None:
+        self._data = data
+        self._prev = prev
+
+    def __iter__(self) -> Iterator[D]:
+        node = self
+        while node:
+            yield node._data
+            node = node._prev.get()
+        yield node._data
+
+    def __bool__(self) -> bool:
+        return self._prev != MB()
+
+    def data_eq(self, other: _SENode[D]) -> bool:
+        """Return true if other node has same or equal data."""
+        if self._data is other._data:
+            return True
+        if self._data == other._data:
+            return True
+        return False
+
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, type(self)):
+            return False
+
+        if self._prev == other._prev and self.data_eq(other):
+                return True
+        return False
+
+    def get_data(self) -> D:
+        """Return contained data"""
+        return self._data
+
+    def fold[T](self, f: Callable[[T, D], T], init: T | None = None) -> T:
+        """Reduce data across linked nodes.
+
+        * with a function and an optional starting value
+        * reduces in natural LIFO order
+          * from self to the root
+
+        """
+        if init is None:
+            acc: T = cast(T, self._data)
+            node = self._prev.get()
+        else:
+            acc = init
+            node = self
+
+        while node:
+            acc = f(acc, node._data)
+            node = node._prev.get()
+        acc = f(acc, node._data)
+        return acc
+
+    def pop2(self) -> tuple[D, MB[_SENode[D]]]:
+        """Return the data in the *head* and potential *tail*."""
+        return self._data, self._prev
+
+    def push_data(self, data: D) -> _SENode[D]:
+        """Push data onto the queue and return a new node containing the data."""
+        return _SENode(data, MB(self))
---
+++ src/dtools/queues/splitends/splitend.py
@@ -0,0 +1,170 @@
+# Copyright 2023-2025 Geoffrey R. Scheller
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+### SplitEnd stack related data structures
+
+With use I am finding this data structure needs some sort of supporting
+infrastructure. Hence I split the original splitend module out to be its own
+subpackage.
+
+#### SplitEnd Stack type and SE factory function
+
+* class SplitEnd: Singularly linked stack with shareable data nodes
+* function SE: create SplitEnd from a variable number of arguments
+
+"""
+
+from __future__ import annotations
+
+from collections.abc import Callable, Iterator
+from typing import Never, TypeVar
+from dtools.fp.err_handling import MB
+from ._splitend_node import _SENode
+
+__all__ = ('SplitEnd',)
+
+D = TypeVar('D')  # Not needed for mypy, hint for pdoc.
+T = TypeVar('T')
+
+
+class SplitEnd[D]:
+    """Class SplitEnd
+
+    LIFO stacks which can safely share immutable data between themselves.
+
+    * each SplitEnd is a very simple stateful (mutable) LIFO stack
+    * data can be pushed and popped to the stack
+    * different mutable split ends can safely share the same "tail"
+    * each SplitEnd sees itself as a singularly linked list
+    * bush-like datastructures can be formed using multiple SplitEnds
+    * len() returns the number of elements on the SplitEnd stack
+    * in boolean context, return true if split end is not empty
+
+    """
+
+    __slots__ = '_count', '_top'
+
+    def __init__(self, *ds: D) -> None:
+        self._count = 0
+        self._top: MB[_SENode[D]] = MB()
+        for d in ds:
+            node = _SENode(d, self._top)
+            self._top, self._count = MB(node), self._count + 1
+
+    def __iter__(self) -> Iterator[D]:
+        if self._top == MB():
+            empty: tuple[D, ...] = ()
+            return iter(empty)
+        return iter(self._top.get())
+
+    def __reversed__(self) -> Iterator[D]:
+        return reversed(list(self))
+
+    def __bool__(self) -> bool:
+        # Returns true if not a root node
+        return bool(self._top)
+
+    def __len__(self) -> int:
+        return self._count
+
+    def __repr__(self) -> str:
+        return 'SplitEend(' + ', '.join(map(repr, reversed(self))) + ')'
+
+    def __str__(self) -> str:
+        return '>< ' + ' -> '.join(map(str, self)) + ' ||'
+
+    def __eq__(self, other: object, /) -> bool:
+        if not isinstance(other, type(self)):
+            return False
+
+        if self._count != other._count:
+            return False
+        if self._count == 0:
+            return True
+
+        left = self._top.get()
+        right = other._top.get()
+        for _ in range(self._count):
+            if left is right:
+                return True
+            if not left.data_eq(right):
+                return False
+            if left:
+                left = left._prev.get()
+                right = right._prev.get()
+
+        return True
+
+    def push(self, *ds: D) -> None:
+        """Push data onto the top of the SplitEnd."""
+        for d in ds:
+            node = _SENode(d, self._top)
+            self._top, self._count = MB(node), self._count + 1
+
+    def pop(self, default: D | None = None, /) -> D | Never:
+        """Pop data off of the top of the SplitEnd.
+
+        * raises ValueError if
+          * popping from an empty SplitEnd
+          * and no default value was given
+
+        """
+        if self._count == 0:
+            if default is None:
+                raise ValueError('SE: Popping from an empty SplitEnd')
+            return default
+
+        data, self._top, self._count = self._top.get().pop2() + (self._count - 1,)
+        return data
+
+    def peak(self, default: D | None = None, /) -> D:
+        """Return the data at the top of the SplitEnd.
+
+        * does not consume the data
+        * raises ValueError if peaking at an empty SplitEnd
+
+        """
+        if self._count == 0:
+            if default is None:
+                raise ValueError('SE: Popping from an empty SplitEnd')
+            return default
+
+        return self._top.get().get_data()
+
+    def copy(self) -> SplitEnd[D]:
+        """Return a copy of the SplitEnd.
+
+        * O(1) space & time complexity.
+        * returns a new instance
+
+        """
+        se: SplitEnd[D] = SplitEnd()
+        se._top, se._count = self._top, self._count
+        return se
+
+    def fold[T](self, f: Callable[[T, D], T], init: T | None = None, /) -> T | Never:
+        """Reduce with a function.
+
+        * folds in natural LIFO Order
+
+        """
+        if self._top != MB():
+            return self._top.get().fold(f, init)
+
+        if init is not None:
+            return init
+
+        msg = 'SE: Folding empty SplitEnd but no initial value supplied'
+        raise ValueError(msg)
