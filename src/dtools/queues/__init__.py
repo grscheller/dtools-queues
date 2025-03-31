658c0c8666b8b1f203154077436f1cb5f0aa30d8
grscheller <geoffrey@scheller.com>
2025-03-30 21:21:10 -0600

    Fixed typo in src/dtools/queues/splitends/__init__.py

--- src/dtools/queues/__init__.py
+++ src/dtools/queues/__init__.py
@@ -28,7 +28,7 @@
 
 """
 
-__version__ = '0.26.0.0'
+__version__ = '0.26.0.1'
 __author__ = 'Geoffrey R. Scheller'
 __copyright__ = 'Copyright (c) 2023-2025 Geoffrey R. Scheller'
 __license__ = 'Apache License 2.0'
